511b92f28b16f546f25c605e3497baa03c2ce3d4
Robby Hill <88348635+robbyhill20@users.noreply.github.com>
2022-03-07 20:24:52 -0800

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/rate-my-portfolio-app',
+  process.env.MONGODB_URI || 'rate-my-portfolio-1.herokuapp.com',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
